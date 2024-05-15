58a613965d75fd1647e99a5286f82aae383133d7
Simon Emms <simon@simonemms.com>
2024-05-15 10:46:10 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# sveltekit-content
+
+Content driven website using SvelteKit
+
+<!-- toc -->
+
+* [Contributing](#contributing)
+  * [Open in Gitpod](#open-in-gitpod)
+  * [Open in a container](#open-in-a-container)
+
+<!-- Regenerate with "pre-commit run -a markdown-toc" -->
+
+<!-- tocstop -->
+
+## Contributing
+
+### Open in Gitpod
+
+* [Open in Gitpod](https://gitpod.io/from-referrer/)
+
+### Open in a container
+
+* [Open in a container](https://code.visualstudio.com/docs/devcontainers/containers)
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		host: process.env.VITE_HOST,
+		port: Number(process.env.VITE_PORT ?? 5173),
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+});
